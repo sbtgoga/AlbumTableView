1f3a4accf7b4b7b5ab6c13d7818c49243e36c839
Sebastian Gonzalez <sbt.goga@gmasil.com>
2025-05-09 17:28:15 -0400

    Fetched API shows on TableView

--- app/src/main/java/com/cinncinatiai/albumtableview/MainActivity.kt
+++ app/src/main/java/com/cinncinatiai/albumtableview/MainActivity.kt
@@ -1,20 +1,30 @@
 package com.cinncinatiai.albumtableview
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.cinncinatiai.albumtableview.ViewModel.MainUIState
+import com.cinncinatiai.albumtableview.ViewModel.MainViewModel
 
 class MainActivity : AppCompatActivity() {
+    private val viewModel: MainViewModel by lazy {
+        MainViewModel()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        viewModel.onInit()
+        viewModel.uiState.observe(this) { OnUIState(it) }
+    }
+}
+
+private fun OnUIState(uiState: MainUIState) {
+    when (uiState) {
+        is MainUIState.AlbumLoaded -> Log.i("MainActivity", uiState.album.toString())
+        MainUIState.Loading -> {} //NOOP
+        MainUIState.None -> {} //NOOP
     }
 }
---
+++ app/src/main/java/com/cinncinatiai/albumtableview/Network/RetrofitModule.kt
@@ -0,0 +1,14 @@
+package com.cinncinatiai.albumtableview.Network
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitClient {
+    private const val BASE_URL = "https://jsonplaceholder.typicode.com"
+
+    val api = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+}
