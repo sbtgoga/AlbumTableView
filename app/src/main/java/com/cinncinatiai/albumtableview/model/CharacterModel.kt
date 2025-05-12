cd7cee0481764736b7322472c31fe6799853adf3
nahunp <nahun.perezv@gmail.com>
2025-05-12 17:25:39 -0400

    Character Model Updated

--- app/src/main/java/com/cinncinatiai/albumtableview/model/CharacterModel.kt
+++ app/src/main/java/com/cinncinatiai/albumtableview/model/CharacterModel.kt
@@ -7,10 +7,14 @@ data class CharacterModel(
     val species: String,
     val type: String,
     val gender: String,
-    val origin: Location,
+    val origin: Origin,
     val location: Location,
     val image: String,
     val episode: List<String>
+)
+data class Origin(
+    val name: String,
+    val url: String
 )
 
 data class Location(
